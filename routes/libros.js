e5bc992816e2057571c8e0ae1fe61b70972678cc
juanmanuelco <juanmanuelco@yahoo.es>
2017-03-12 15:31:59 -0500

    obtener

--- routes/libros.js
+++ routes/libros.js
@@ -6,50 +6,7 @@ var multer = require('multer'),
     var Libro = require('../models/libros');
 
 cloudinary.config({cloud_name: 'packbooks', api_key: '422278622363686', api_secret: 'JCX3Zpg0p8LkG-h6IiBpRZqZOjo'});
-/*
-router.post('/subida',function(req,res){
-    var nombre=req.body.nombre;
-    var autor=req.body.autor;
-    var editorial=req.body.editorial;
-    var publicacion=req.body.publicacion;
-    var categoria=req.body.categoria;
-    var por=req.body.subidoPor;
-    var tipo=req.body.tipo;
-    console.log(req.body)
-    var storage = multer.diskStorage({
-	destination: function (req, file, cb) {cb(null, 'public/'+tipo)},
-        filename: function (req, file, cb) { cb(null, por+'.pdf');}
-    });
 
-    var upload = multer({ storage: storage, fileFilter:function(req, file, cb){
-            if(file.mimetype=='application/pdf'){cb(null, true);}else{cb(null, false);}
-        }}).single('book');
-    upload(req, res, function (err) {
-        if(err){res.send('Error 1')}     
-        else{
-            cloudinary.uploader.upload('public/'+tipo+'/'+por+'.pdf',
-                function(result) {
-                    var libroSchema=new Libro({
-                        id:nombre+'-'+por+'-'+Date.now(),
-                        nombre: nombre,
-                        autor:autor,
-                        editorial:editorial,
-                        fechaPub:publicacion,
-                        fechaSub:Date.now(),
-                        categoria:categoria,
-                        subidoPor:por,
-                        libro:result.url,
-                        tipo:tipo
-                    });
-                    libroSchema.save(function(err){
-                        if(err){res.send('Error 1')}else{res.send('ok')}
-                    })
-                },{public_id: tipo+'/'+nombre+'-'+por+'-'+Date.now()+'.pdf'} 
-            )
-        } 
-    });
-});
-*/
 //Crea una oferta__________________________________________________________________________
 router.post('/subida',function(req,res){
     var storage = multer.diskStorage({
@@ -60,16 +17,13 @@ router.post('/subida',function(req,res){
         if(file.mimetype=='application/pdf'){cb(null, true);}else{cb(null, false);}
     }}).single('book');
     upload(req, res, function (err) {
-        if(err){res.render('500',{error:err})}
+        if(err){res.send(err)}
         else{
             cloudinary.uploader.upload(
                 "public/"+req.body.tipo+"/"+req.body.por+".pdf",
                 function(result) { 
-                    console.log(result);
-                    var imagenOferta=result.url;
-                    console.log(req.body)
                      var libroSchema=new Libro({
-                        id:req.body.nombre+'-'+req.body.por,
+                        id:req.body.nombre+'-'+req.body.por+'-'+ new Date(),
                         nombre: req.body.nombre,
                         autor:req.body.autor,
                         editorial:req.body.editorial,
@@ -85,6 +39,12 @@ router.post('/subida',function(req,res){
                     })
             },{public_id: req.body.tipo+'/'+req.body.por});
         }
+    });
+});
+
+router.get('/obtener-publicos',function(req,res){
+    Libro.find().exec(function(err,resp){
+        res.send(resp);
     });
 });
 
